b3a790e0868b535896cdc091c74bbf1d81f63878
Tatas Fachrul <tatasfachrul@gmail.com>
2019-11-20 16:58:01 +0700

    create interface

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,29 @@
-const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const cssnano = require("cssnano");
-const autoprefixer = require("autoprefixer");
+const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
 class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:\/]+/g);
+  static extract (content) {
+    return content.match(/[A-z0-9-:\/]+/g)
   }
 }
 
 module.exports = {
   plugins: [
-    tailwindcss("tailwind.config.js"),
+    tailwindcss('tailwind.config.js'),
     cssnano({
-      preset: "default"
+      preset: 'default'
     }),
     purgecss({
-      content: ["src/**/*.html"],
+      content: ['src/**/*.html'],
       extractors: [
         {
           extractor: TailwindExtractor,
-          extensions: ["html", "js", "php", "vue"]
+          extensions: ['html', 'js', 'php', 'vue']
         }
       ]
     }),
     autoprefixer
   ]
-};
+}
